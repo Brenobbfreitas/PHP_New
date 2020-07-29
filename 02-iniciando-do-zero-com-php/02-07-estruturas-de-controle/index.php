b6ff55c8cd7393c2a861098f4e0b25c640c528f9
brenobbfreitas <brenofreitas0000@gmail.com>
2020-07-29 00:17:58 -0300

    estruturas de repeticao

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,13 +9,54 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+$teste = true;
 
+if($teste){
+    var_dump(true);
+} else{
+    var_dump(false);
+}
+
+$age = 2;
+
+if($age < 18){
+    var_dump("pode tirar abilitação");
+} elseif ($age >= 21){
+    var_dump("pode consumir bebida alcoolica");
+} else{
+    var_dump("maior de idade");
+}
+
+$hour = 3;
+if ($hour >= 5 || $hour < 23){
+    if ($hour < 7 ){
+        var_dump("bob marley");
+    }else{
+        var_dump("after bridge");
+    }
+}else{
+    var_dump("ZZZZzzzzzZZZZ");
+}
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
  * [ empty] https://php.net/manual/pt_BR/function.empty.php
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+$rock = "";
+
+if (!isset($rock)){
+    var_dump("Rock and Roll");
+}else{
+    var_dump("die");
+}
+
+$rockandroll = "ACDC";
+if (!empty($rockandroll)){
+    var_dump("Rock existe e toca {$rockandroll}");
+}else{
+    var_dump("Não existe ou não está tocando");
+}
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
@@ -23,5 +64,24 @@ fullStackPHPClassSession("isset, empty, !", __LINE__);
 fullStackPHPClassSession("switch", __LINE__);
 
 
+$payment = "canceled";
 
-
+switch ($payment){
+    case "billet_printed":
+        break;
+        var_dump("boleto impresso");
+    case 'canceled':
+        break;
+        var_dump("pagamento cancelado");
+    case 'past_due':
+    case 'pending':
+        var_dump("aguardando pagamento");
+        break;
+    case 'aproved':
+    case 'completed':
+        var_dump("pagamento aprovado");
+        break;
+    default:
+        var_dump("erro ao processar pagamento");
+        break;
+};
--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -8,12 +8,36 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+$while = [];
+$looping = 1;
 
+while($looping <= 5){
+    $while[] = $looping;
+    $looping++;
+}
+var_dump($while);
+
+$while = [];
+$looping = 5;
+
+do {
+    $while[] = $looping;
+    $looping--;
+}while($looping >= 1);
+
+    var_dump($while);
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
 
+for ($i = 1; $i <=10 ; $i++){
+    if($i % 2 == 0){
+        echo "<p>o valor é par{$i}</p>";
+    }else{
+    echo "<p>o valor é impar {$i}</p>";
+    }
+};
 
 /**
  * [ break ] https://php.net/manual/pt_BR/control-structures.break.php
@@ -21,8 +45,32 @@ fullStackPHPClassSession("for", __LINE__);
  */
 fullStackPHPClassSession("break, continue", __LINE__);
 
+for ($c = 1; $c <= 10; $c++){
+    if($c % 2 == 1){
+        continue;
+    }
+    if ($c >= 10){
+        break;
+    }
+    echo "<p>Pulou + 2 ::{$c}</p>";
+}
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$array = [];
+for ($ar = 0; $ar <= 5; $ar++){
+    $array[] = $ar;
+}
+
+var_dump($array);
+
+foreach ($array as $item){
+    var_dump($item);
+}
+
+foreach ($array as $key => $value){
+    var_dump("{$key} = {$value}");
+}
