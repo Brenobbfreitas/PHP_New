b6b72565872c3ea4e7714a2a7dcab9a1fbc03b56
brenobbfreitas <brenofreitas0000@gmail.com>
2020-08-20 18:27:17 -0300

    finalizando closure e generator

--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -8,11 +8,57 @@ fullStackPHPClassName("02.09 - Closures e generators");
 fullStackPHPClassSession("closures", __LINE__);
 
 
-$value = 20;
-var_dump($value);
-echo "<p>value é igual a 20</p>";
-echo "<p> value é igual {$value}</p>";
+$person = function($name){
+    printf("Hello %s \r  \n", $name);
+} ;
+
+$person('Breno');
+$person('Barreto');
+
+$myAge = function($year){
+    $age = date("Y") - $year;
+    return "<p> Voce tem {$age} anos! </p>";
+};
+
+echo $myAge(1999);
+
+
+$priceBrl = function($price){
+    return number_format($price, 2, ",", ".");
+};
+
+echo "<p>O projeto custa R$ {$priceBrl(3600)}. Vamos fechar ?";
+
+$myCart = [];
+$myCart["totalPrice"] = 0;
+$cartAdd = function ($item , $qtd, $price) use (&$myCart){
+    $myCart[$item] = $qtd * $price;
+    $myCart["totalPrice"] += $myCart[$item];
+};
+
+$cartAdd("Html5", 2, 500);
+
+var_dump($myCart);
+
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
  */
 fullStackPHPClassSession("generators", __LINE__);
+
+
+$iterator = 4000
+
+
+function generatorDate($days){
+    
+    for($day = 1; $day < $days; $day++){
+        yield date("d/m/y", strtotime("+{$day}days"));
+    }
+}
+
+echo "<div style='text-aling: center'>";
+foreach (generatorDate($iterator) as $date){
+    echo "<small class='tag'>($date)</small>". PHP_EOL;
+}   
+
+echo "</div>";
