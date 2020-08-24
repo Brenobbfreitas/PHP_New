014ba470c15c5a03e081beb32f462fda8f9a4575
brenobbfreitas <brenofreitas0000@gmail.com>
2020-08-24 17:53:52 -0300

    require/includ

--- 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -7,11 +7,30 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  * [ include_once ] https://php.net/manual/pt_BR/function.include-once.php
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
+//include "header.php";
+include __DIR__. "/header.php";
+
+$profile = new StdClass();
+$profile->name = "robson";
+$profile->company = "tjpb";
+$profile->email = "robson@gmail.com";
+include __DIR__. "/profile.php";
+
+$profile = new StdClass();
+$profile->name = "gabriel";
+$profile->company = "Google";
+$profile->email = "Gabriel@gmail.com";
+include __DIR__. "/profile.php";
 
 
+//include_once: geralmente usado para inserir cofigurações; n]ap gera conflito
+var_dump($profile);
 /*
  * [ require ] https://php.net/manual/pt_BR/function.require.php
  * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
 
+require __DIR__ . "/config.php";
+
+echo "<h1>". Course ."</h1>";
